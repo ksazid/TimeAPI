41c3780cf069a6474f83cce29e5bc70afbe67561
Hifza Kausar <sazid@interfuture.ae>
2019-11-12 12:21:54 +0400

    Chnge 1

--- TimeAPI.Test/UnitController.cs
+++ TimeAPI.Test/UnitController.cs
@@ -11,11 +11,11 @@ namespace TimeAPI.Test
     [TestClass]
     public class UnitControllerx
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IEmailSender _emailSender;
-        private readonly ILogger _logger;
-        private readonly ApplicationSettings _appSettings;
+        //private readonly UserManager<ApplicationUser> _userManager;
+        //private readonly SignInManager<ApplicationUser> _signInManager;
+        //private readonly IEmailSender _emailSender;
+        //private readonly ILogger _logger;
+        //private readonly ApplicationSettings _appSettings;
 
         [TestMethod]
         public void TestMethod1()
