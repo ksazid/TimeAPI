0ce5f46496213742838fe29c9a56eb103528cf2b
Hifza Kausar <sazid@interfuture.ae>
2019-12-27 02:19:17 +0400

    Encode remove

--- TimeAPI.API/Controllers/AccountController.cs
+++ TimeAPI.API/Controllers/AccountController.cs
@@ -330,15 +330,15 @@ namespace TimeAPI.API.Controllers
         }
 
 
-        public static string EncodeServerName(string serverName)
-        {
-            return Convert.ToBase64String(Encoding.UTF8.GetBytes(serverName));
-        }
+        //public static string EncodeServerName(string serverName)
+        //{
+        //    return Convert.ToBase64String(Encoding.UTF8.GetBytes(serverName));
+        //}
 
-        public static string DecodeServerName(string encodedServername)
-        {
-            return Encoding.UTF8.GetString(Convert.FromBase64String(encodedServername));
-        }
+        //public static string DecodeServerName(string encodedServername)
+        //{
+        //    return Encoding.UTF8.GetString(Convert.FromBase64String(encodedServername));
+        //}
         #endregion
     }
 }
