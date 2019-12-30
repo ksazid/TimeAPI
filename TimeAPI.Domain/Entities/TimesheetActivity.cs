fb2f4e795b03f946df7cf3f47564c0584ceb2173
Hifza Kausar <sazid@interfuture.ae>
2019-12-30 13:59:08 +0400

    Timesheet Chaqnges

---
+++ TimeAPI.Domain/Entities/TimesheetActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeAPI.Domain.Entities
+{
+    public class TimesheetActivity
+    {
+        public string id { get; set; } 
+        public string groupid { get; set; }
+        public string task_id { get; set; }
+        public string subtask_id { get; set; }
+        public string remarks { get; set; }
+        public string ondate { get; set; }
+        public string start_time { get; set; }
+        public string end_time { get; set; }
+        public string created_date { get; set; }
+        public string createdby { get; set; }
+        public string modified_date { get; set; }
+        public string modifiedby { get; set; }
+        public bool is_deleted { get; set; }
+
+    }
+}
---
+++ TimeAPI.Domain/Entities/TimesheetActivityComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeAPI.Domain.Entities
+{
+    public class TimesheetActivityComment
+    {
+        public string id { get; set; } 
+        public string groupid { get; set; }
+        public string task_id { get; set; }
+        public string subtask_id { get; set; }
+        public string comments { get; set; }
+        public string created_date { get; set; }
+        public string createdby { get; set; }
+        public string modified_date { get; set; }
+        public string modifiedby { get; set; }
+        public bool is_deleted { get; set; }
+
+    }
+}
