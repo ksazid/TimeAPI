1a78661de82fd599d21dbe29d7a52bf46b2add1f
Hifza Kausar <sazid@interfuture.ae>
2020-01-12 16:20:35 +0400

    Test SMS 2

--- TimeAPI.API/Controllers/AccountController.cs
+++ TimeAPI.API/Controllers/AccountController.cs
@@ -163,9 +163,9 @@ namespace TimeAPI.API.Controllers
             #region
 
             // //.Decode(code);
-            var xcode = Base64UrlEncoder.Decode(code);
+            //var xcode = Base64UrlEncoder.Decode(code);
             //var _code = HttpUtility.UrlDecode(xcode);
-            IdentityResult identityResult = await _userManager.ConfirmEmailAsync(user, xcode).ConfigureAwait(true);
+            IdentityResult identityResult = await _userManager.ConfirmEmailAsync(user, code).ConfigureAwait(true);
 
             try
             {
