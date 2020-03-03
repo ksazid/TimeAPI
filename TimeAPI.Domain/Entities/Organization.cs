d70fb7f9964135c914b570c99240dcfb2098dc82
Sazid <sazid@interfuture.ae>
2020-03-03 14:33:49 +0400

    parent_org_id

--- TimeAPI.Domain/Entities/Organization.cs
+++ TimeAPI.Domain/Entities/Organization.cs
@@ -25,6 +25,7 @@ namespace TimeAPI.Domain.Entities
         public string modified_date { get; set; }
         public string modifiedby { get; set; }
         public bool is_deleted { get; set; }
+        public string parent_org_id { get; set; }
         public EntityLocation EntityLocation { get; set; }
         public OrganizationSetup OrganizationSetup { get; set; }
         public List<OrganizationBranchViewModel> OrganizationBranchViewModel { get; set; }
